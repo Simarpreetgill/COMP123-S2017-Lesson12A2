511dc85c0a26d91861238b031ee43a5971be7ba7
300927812 <300927812@PR-A315-24>
2017-08-03 10:44:33 -0400

    Added calculator

---
+++ COMP123-S2017-Lesson12A2/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Name- Simarpreet Kaur
+ * Date - August 3,2017
+ * Description- A calculator app  with windows form
+ */
+namespace COMP123_S2017_Lesson12A2
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ COMP123-S2017-Lesson12A2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Name- Simarpreet Kaur
+ * Date - August 3,2017
+ * Description- A calculator app  with windows form
+ */
+namespace COMP123_S2017_Lesson12A2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
